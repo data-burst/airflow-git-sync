5433e78d8e62f48d5819dadaf598fb2b9e137bbd
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2023-08-26 21:36:41 +0330

    :seedling: feat(examples): add example DAGs directory for BashOperator and DockerOperator

---
+++ examples/dags/sample_bash_operator_dag.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.docker_operator import DockerOperator
+
+
+default_args = {
+    'owner'                 : 'Mostafa Ghadimi',
+    'description'           : 'Use of the DockerOperator',
+    'depend_on_past'        : False,
+    'start_date'            : datetime(2023, 4, 4),
+    'email_on_failure'      : False,
+    'email_on_retry'        : False,
+    'retries'               : 1,
+    'retry_delay'           : timedelta(minutes=5)
+}
+
+with DAG('docker_dag_sample', default_args=default_args, schedule_interval="5 10 * * *", catchup=False) as dag:
+    t1 = BashOperator(
+        task_id='print_hello',
+        bash_command='echo "hello world"'
+    )
+
+t1
+
