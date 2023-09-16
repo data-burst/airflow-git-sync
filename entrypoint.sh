c2bb5315a4839ae133f4c0a59ea616dd796ba56c
Niyusha Baghayi <niyusha.baghayi@gmail.com>
2023-09-16 16:12:17 +0330

    :bug: fix(entrypoint): resolve git clean issue and use global variable for the project directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
+PROJECT_DIRECTORY="/app/${DIRECTORY_NAME:-project}"
+
 mkdir -p ~/.ssh
 
-if [ ! -d "/app/${DIRECTORY_NAME:-project}/.git" ]; then
+if [ ! -d "$PROJECT_DIRECTORY/.git" ]; then
   echo "Cloning the repository: $REPO_URL"
-  mkdir -p /app/${DIRECTORY_NAME:-project}
+  mkdir -p $PROJECT_DIRECTORY
   ssh-keyscan ${GIT_URL:-github.com} >> ~/.ssh/known_hosts
-  git init /app/${DIRECTORY_NAME:-project}
-  cd /app/${DIRECTORY_NAME:-project}
+  git init $PROJECT_DIRECTORY
+  cd $PROJECT_DIRECTORY
   git remote add origin $REPO_URL
   git pull origin ${GIT_BRANCH:-main}
-  rsync -vazC /app/${DIRECTORY_NAME:-project}/ ${DESTINATION_PATH:-/app/sync}
+  rsync -vazC $PROJECT_DIRECTORY/ ${DESTINATION_PATH:-/app/sync}
+fi
+
+if [[ "$PWD" != "$PROJECT_DIRECTORY" ]]
+then
+    cd "$PROJECT_DIRECTORY"
 fi
 
 while true; do
   echo "Syncing the repository every $INTERVAL seconds"
-  git -C /app/${DIRECTORY_NAME:-project} pull origin ${GIT_BRANCH:-main}
+  git -C $PROJECT_DIRECTORY pull origin ${GIT_BRANCH:-main}
   git clean -fd
   sleep ${INTERVAL:-10}
-  rsync -vazC /app/${DIRECTORY_NAME:-project}/ ${DESTINATION_PATH:-/app/sync}
+  rsync -vazC $PROJECT_DIRECTORY/ ${DESTINATION_PATH:-/app/sync}
 done
 
