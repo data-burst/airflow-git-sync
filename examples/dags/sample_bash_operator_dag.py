d3b53aa8a624c6077218d22157226626c406033f
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2025-08-15 23:42:30 +0330

    :recycle: refactor: improve the example quality for BashOperator

--- examples/dags/sample_bash_operator_dag.py
+++ examples/dags/sample_bash_operator_dag.py
@@ -1,24 +1,22 @@
-from airflow import DAG
 from datetime import datetime, timedelta
+
+from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 
-
 default_args = {
-    'owner'                 : 'Mostafa Ghadimi',
-    'description'           : 'Use of the DockerOperator',
-    'depend_on_past'        : False,
-    'start_date'            : datetime(2023, 4, 4),
-    'email_on_failure'      : False,
-    'email_on_retry'        : False,
-    'retries'               : 1,
-    'retry_delay'           : timedelta(minutes=5)
+    "owner": "Mostafa Ghadimi",
+    "description": "Use of the BashOperator",
+    "depend_on_past": False,
+    "start_date": datetime(2023, 4, 4),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
-with DAG('docker_dag_sample', default_args=default_args, schedule="5 10 * * *", catchup=False) as dag:
-    t1 = BashOperator(
-        task_id='print_hello',
-        bash_command='echo "hello world"'
-    )
+with DAG(
+    "bash_dag_sample", default_args=default_args, schedule="5 10 * * *", catchup=False
+) as dag:
+    t1 = BashOperator(task_id="print_hello", bash_command='echo "hello world"')
 
 t1
-
