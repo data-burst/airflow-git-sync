5155aecf2d11ba1d5a2aba574a79b6a8a1bbe550
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2025-08-16 00:32:30 +0330

    :rocket: feat!: migrate docker compose and examples to the airflow 3 (#31)
    * :bug: fix: import bash operator and resolve it incompatible backward version
    
    * :fire: fix: remove additional error from incompatible import
    
    * :bug: fix: replace schedule with schedule_interval
    
    * :see_no_evil: chore: add config directory to gitignore file
    
    * :recycle: refactor: improve the example quality for BashOperator
    
    * :bug: fix: import DockerOperator for Airflow 3
    
    * :whale: feat: upgrade docker-compose to airflow 3
    
    * :bug: fix: change auto_remove value for DockerOperator to a valid one

--- examples/dags/sample_docker_operator_dag.py
+++ examples/dags/sample_docker_operator_dag.py
@@ -1,30 +1,33 @@
-from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.docker_operator import DockerOperator
 
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
 
 default_args = {
-    'owner'                 : 'Mostafa Ghadimi',
-    'description'           : 'Use of the DockerOperator',
-    'depend_on_past'        : False,
-    'start_date'            : datetime(2023, 4, 23),
-    'email_on_failure'      : False,
-    'email_on_retry'        : False,
-    'retries'               : 1,
-    'retry_delay'           : timedelta(minutes=5)
+    "owner": "Mostafa Ghadimi",
+    "description": "Use of the DockerOperator",
+    "depend_on_past": False,
+    "start_date": datetime(2023, 4, 23),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
-with DAG('docker_dag_v2', default_args=default_args, schedule_interval="5 10 * * *", catchup=False) as dag:
+with DAG(
+    "docker_dag_v2",
+    default_args=default_args,
+    schedule="5 10 * * *",
+    catchup=False,
+) as dag:
     t1 = DockerOperator(
-        task_id='docker_command',
-        image='alpine:latest',
-        api_version='auto',
-        auto_remove=True,
+        task_id="docker_command",
+        image="alpine:latest",
+        api_version="auto",
+        auto_remove="success",
         command="/bin/sleep 30",
         docker_url="unix://var/run/docker.sock",
-        network_mode="bridge"
+        network_mode="bridge",
     )
 
 t1
-
